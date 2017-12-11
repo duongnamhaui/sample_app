0e8f91015f2a61d5afe232c735a6b0bbb2ffdecc
duongnamhaui <duongnamhaui@gmail.com>
2017-12-11 13:43:31 +0700

    Finish sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
   has_secure_password
-  validates :password, presence: true, length: {minimum: Settings.password.minimum}
+  validates :password, presence: true,
+    length: {minimum: Settings.password.minimum}
 end
